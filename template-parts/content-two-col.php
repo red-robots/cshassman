776e790d9acada2d242516f840535ac51fe33bca
fritzhealy <fre.healy@gmail.com>
2018-06-11 08:55:05 -0400

    templating and styling two col layout

---
+++ template-parts/content-two-col.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ACStarter
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class("template-two-col"); ?>>
+	<?php $banner = get_field("banner");
+	if($banner):?>
+		<div class="row-1">
+			<img src="<?php echo $banner['url'];?>" alt="<?php echo $baner['alt'];?>">
+		</div><!--.row-1-->
+	<?php endif;?>
+	<div class="wrapper cap row-2">
+		<header>
+			<h1><?php the_title();?></h1>
+		</header>
+		<div class="wrapper clear-bottom">
+			<div class="col-1 copy">
+				<?php the_content();?>
+			</div><!--.col-1-->
+			<div class="col-2">
+				<?php $image = get_field("image");
+				if($image):?>
+					<img src="<?php echo $image['url'];?>" alt="<?php echo $image['alt'];?>">
+				<?php endif;?>
+			</div><!--.col-2-->
+		</div><!--.wrapper-->
+	</div><!--.wrapper-->
+</article><!-- #post-## -->
