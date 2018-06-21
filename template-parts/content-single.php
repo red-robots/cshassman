08d5bb0066987f7f290d377e95e00fb3364f5529
Austin Crane <austincrane@gmail.com>
2018-06-21 16:51:13 -0400

    take off archives

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -46,14 +46,14 @@
 				<?php $limit = 0;
 				$current_year = date('Y');
 				$rows = $wpdb->get_results("SELECT DISTINCT YEAR( post_date ) AS year FROM $wpdb->posts WHERE post_status = 'publish' and post_date <= now( ) and post_type = 'post' ORDER BY post_date DESC");
-				if($rows):?>
+				if($rows){?>
 					<ul>
 						<?php foreach($rows as $row) :
 							if(strcmp($row->year,$current_year)===0) continue;?>
 							<li class="archive-year"><a href="<?php bloginfo('url') ?>/<?php echo $row->year; ?>/"><?php echo $row->year;?></a></li>
 						<?php endforeach;?>
 					</ul> 
-				<?php endif;?>
+				<?php } ?>
 				<?php endif;?>
 
 				<?php $categories_title = get_field("categories_title","option");
